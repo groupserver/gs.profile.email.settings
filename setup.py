5cd806ad916dfee549050f5a9e0fb21fb66ad29d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 15:01:05 -0700

    Updating the egg deps.

--- setup.py
+++ setup.py
@@ -12,39 +12,20 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
+import sys
 from version import get_version
 
 version = get_version()
 
-setup(name='gs.profile.email.settings',
-    version=version,
-    description="Update email address settings on GroupServer.",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-    keywords='profile email address add remove groupserver',
-    author='Alice Murphy',
-    author_email='alice@onlinegroups.net',
-    url='http://groupserver.org/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.profile', 'gs.profile.email'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
+requires = [
         'zope.browsermenu',
         'zope.browserpage',
         'zope.browserresource',
@@ -61,11 +42,42 @@ setup(name='gs.profile.email.settings',
         'gs.content.js.jquery.base',
         'gs.content.layout',
         'gs.core',
+        'gs.group.member.email.base',
         'gs.profile.base',
         'gs.profile.email.base',
         'gs.profile.email.verify',
         'Products.CustomUserFolder',
-    ],
+    ]
+if (sys.version_info < (3, 4)):
+    requires += ['setuptools', ]
+
+setup(name='gs.profile.email.settings',
+    version=version,
+    description="Update email address settings on GroupServer.",
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='profile, email, email address, settings,groupserver',
+    author='Alice Murphy',
+    author_email='alice@onlinegroups.net',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
+    url='https://source.iopen.net/groupserver/gs.profile.email.settings/',
+    license='ZPL 2.1',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['gs', 'gs.profile', 'gs.profile.email'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
     entry_points="""
     # -*- Entry points: -*-
     """,)
